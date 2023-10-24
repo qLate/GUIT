061ca0d1484f6859819f9c61bf0e43284c78b26e
qLate <uaolegikua@gmail.com>
2023-10-24 21:32:50 +0300

    init

---
+++ CMakeFiles/HelloWorld.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/hgfs/C/Users/Oleg/Documents/Programming/OS/hello_wayland/hello_wayland.c" "CMakeFiles/HelloWorld.dir/hello_wayland.c.o" "gcc" "CMakeFiles/HelloWorld.dir/hello_wayland.c.o.d"
+  "/mnt/hgfs/C/Users/Oleg/Documents/Programming/OS/hello_wayland/helpers.c" "CMakeFiles/HelloWorld.dir/helpers.c.o" "gcc" "CMakeFiles/HelloWorld.dir/helpers.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
