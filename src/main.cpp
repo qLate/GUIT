800ed796ab0bd267ae2e6d75ad1fead849cb6669
sviat-l <sviatoslav.lushnei@ucu.edu.ua>
2023-11-26 15:14:32 +0200

    ;

--- src/main.cpp
+++ src/main.cpp
@@ -253,3 +253,30 @@ int main(int argc, char **argv) {
 
     exit(0);
 }
+
+
+//#include "GUIToolkit.h"
+//#include "Component.h"
+//#include "WindowW.h"
+//#include "SubComponent.h"
+//
+//#include "cairo/cairo-gl.h"
+
+//int main()
+//{
+//    GUIToolkit toolkit {};
+//
+//    WindowW w1 {"Test window", {1, 1}};
+//    w1.setImage("../images/sviat.jpg");
+//    w1.resize({500, 500});
+//
+//    SubComponent s1 {&w1, {w1.size.x/2, 100}};
+//    s1.moveWindowOnDrag = true;
+//    s1.setAnchors({0, 0.5f}, {1, 0.5f});
+//    s1.setColor(Color::blue());
+//
+//
+//    toolkit.loop();
+//
+//    return 0;
+//}
